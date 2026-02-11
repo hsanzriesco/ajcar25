293a5f354c55b5853c77e52ff6a16bfe0f0b8f26
hsanzriesco <hsanzriesco@gmail.com>
2026-02-11 18:46:44 +0100

    Prueba de eliminacion de navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/layout/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +29,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-900 text-white`}
       >
-        <Navbar />
         {children}
       </body>
     </html>
